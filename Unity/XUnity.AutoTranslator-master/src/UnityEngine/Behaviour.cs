f6e10c044daf1195aa9951f21036177fb6e2262d
Chanomhub <121768693+Chanomhub@users.noreply.github.com>
2024-02-21 16:33:16 +0700

    Add files via upload

---
+++ Unity/XUnity.AutoTranslator-master/src/UnityEngine/Behaviour.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+
+namespace UnityEngine
+{
+   public class Behaviour : Component
+   {
+      public Behaviour() : base( IntPtr.Zero ) => throw new NotImplementedException();
+
+      public bool enabled
+      {
+         get => throw new NotImplementedException();
+         set => throw new NotImplementedException();
+      }
+
+      public bool isActiveAndEnabled => throw new NotImplementedException();
+   }
+}
