2c5d6cd87e4c8ec4107de8410b3edb23b1a8058d
Chanomhub <121768693+Chanomhub@users.noreply.github.com>
2024-03-24 15:50:27 +0700

    Update main.go

--- RPGM/Chanomhub-RPGM/main.go
+++ RPGM/Chanomhub-RPGM/main.go
@@ -1,92 +1,74 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"os"
-	"path/filepath"
 
 	gt "github.com/bas24/googletranslatefree"
 	"github.com/minio/simdjson-go"
 )
 
+// Function to perform the translation
 func translateText(text, sourceLang, targetLang string) (string, error) {
 	return gt.Translate(text, sourceLang, targetLang)
 }
 
-func translateJSON(filePath, sourceLang, targetLang string, fieldsToTranslate []string) error {
-	// Read JSON file
-	data, err := ioutil.ReadFile(filePath)
-	if err != nil {
-		return err
-	}
-
-	// Parse JSON
-	parsed, err := simdjson.Parse(data, nil)
-	if err != nil {
-		return err
-	}
-
-	// Traverse and translate specified fields
-	for _, field := range fieldsToTranslate {
-		val, err := parsed.SearchKeyInsensitive([]byte(field))
-		if err == nil && val.Type == simdjson.TypeString {
-			translated, err := translateText(string(val.IterBytes()), sourceLang, targetLang)
-			if err != nil {
-				return err
-			}
-			err = parsed.SetBytes([]byte(translated), []string{field})
-			if err != nil {
-				return err
-			}
-		}
-	}
-
-	// Marshal the updated JSON
-	updatedJSON, err := parsed.MarshalJSON()
-	if err != nil {
-		return err
-	}
-
-	// Write the updated JSON to a new file
-	outputPath := filepath.Join("output", filepath.Base(filePath))
-	err = ioutil.WriteFile(outputPath, updatedJSON, 0644)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
 func main() {
-	const (
-		sourceLang      = "en"
-		targetLang      = "es"
-		inputFolder     = "input"
-		fieldsToTranslate = []string{"title", "description"} // specify fields to translate
-	)
+	// --- Example Usage ---
 
-	// Create output directory if it doesn't exist
-	outputDir := "output"
-	if _, err := os.Stat(outputDir); os.IsNotExist(err) {
-		os.Mkdir(outputDir, 0755)
-	}
-
-	// Get list of JSON files in input folder
-	fileList, err := filepath.Glob(filepath.Join(inputFolder, "*.json"))
+	// 1. Read original JSON file
+	originalData, err := ioutil.ReadFile("input.json")
 	if err != nil {
-		fmt.Println("Error:", err)
+		fmt.Println("Error reading input JSON:", err)
 		return
 	}
 
-	// Translate each JSON file
-	for _, file := range fileList {
-		err := translateJSON(file, sourceLang, targetLang, fieldsToTranslate)
-		if err != nil {
-			fmt.Printf("Error translating file %s: %v\n", file, err)
-		} else {
-			fmt.Printf("Translation successful for file %s\n", file)
+	// 2. Parse the JSON data (using simdjson-go)
+	parsed, err := simdjson.Parse(originalData, nil)
+	if err != nil {
+		fmt.Println("Error parsing JSON:", err)
+		return
+	}
+
+	// 3. Iterate and translate specified fields
+	iter := parsed.Iter()
+	for {
+		typ := iter.Advance()
+		switch typ {
+		case simdjson.TypeObject:
+			obj, _ := iter.Object(nil)
+			objIter := obj.Iter()
+		FIELD_LOOP:
+			for {
+				field := objIter.Advance()
+				switch field {
+				case simdjson.TypeString("title"): // Specify the field to translate
+					str, _ := objIter.StringBytes()
+					translated, err := translateText(string(str), "en", "es") // Example translation
+					if err != nil {
+						fmt.Println("Translation error:", err)
+					} else {
+						iter.DelField()          // Remove the original field
+						iter.AddString(field, translated) // Add the translated field
+					}
+				default:
+					break FIELD_LOOP // Skip other fields
+				}
+			}
+		default:
+			// Handle other JSON types if needed
 		}
 	}
+
+	// 4. Write the modified JSON to a new file
+	outputFilename := "output_es.json" // Use language code for clarity
+	outputData := parsed.MarshalJSON()
+	err = ioutil.WriteFile(outputFilename, outputData, 0644)
+	if err != nil {
+		fmt.Println("Error writing output JSON:", err)
+		return
+	}
+
+	fmt.Println("Translation and JSON export complete!")
 }
