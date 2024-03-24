89ac4c4e2db0d0864db1f8f0c43decf6fb4a5ade
Chanomhub <121768693+Chanomhub@users.noreply.github.com>
2024-03-24 15:31:13 +0700

    Update main.go

--- RPGM/Chanomhub-RPGM/main.go
+++ RPGM/Chanomhub-RPGM/main.go
@@ -11,30 +11,30 @@ import (
 )
 
 type EnemyData struct {
-	ID       int      `json:"id"`
-	Animation1Hue int      `json:"animation1Hue"`
-	Animation1Name string    `json:"animation1Name"`
-	Animation2Hue int      `json:"animation2Hue"`
-	Animation2Name string    `json:"animation2Name"`
-	Frames     [][][]float64 `json:"frames"`
-	Name      string    `json:"name"`
-	Position    int      `json:"position"`
-	Timings    []Timings   `json:"timings"`
+	ID            int       `json:"id"`
+	Animation1Hue int       `json:"animation1Hue"`
+	Animation1Name string    `json:"animation1Name"`
+	Animation2Hue int       `json:"animation2Hue"`
+	Animation2Name string    `json:"animation2Name"`
+	Frames        [][][]float64 `json:"frames"`
+	Name          string    `json:"name"`
+	Position      int       `json:"position"`
+	Timings       []Timings `json:"timings"`
 }
 
 type Timings struct {
-	FlashColor  []int `json:"flashColor"`
-	FlashDuration int  `json:"flashDuration"`
-	FlashScope  int  `json:"flashScope"`
-	Frame     int  `json:"frame"`
-	Se      Se  `json:"se"`
+	FlashColor    []int `json:"flashColor"`
+	FlashDuration int   `json:"flashDuration"`
+	FlashScope    int   `json:"flashScope"`
+	Frame         int   `json:"frame"`
+	Se            Se    `json:"se"`
 }
 
 type Se struct {
-	Name  string `json:"name"`
-	Pan  int  `json:"pan"`
-	Pitch int  `json:"pitch"`
-	Volume int  `json:"volume"`
+	Name   string `json:"name"`
+	Pan    int    `json:"pan"`
+	Pitch  int    `json:"pitch"`
+	Volume int    `json:"volume"`
 }
 
 // Define a map to associate file names with struct types
@@ -123,4 +123,3 @@ func translateAndUpdateNames(enemies []EnemyData, targetLang string) {
 		}
 	}
 }
-
