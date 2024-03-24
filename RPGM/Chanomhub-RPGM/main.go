1c45400e37932ff1be656fb545acba3e00d041be
Chanomhub <121768693+Chanomhub@users.noreply.github.com>
2024-03-24 09:29:35 +0700

    Update main.go

--- RPGM/Chanomhub-RPGM/main.go
+++ RPGM/Chanomhub-RPGM/main.go
@@ -10,55 +10,33 @@ import (
 	gt "github.com/bas24/googletranslatefree"
 )
 
-// Define a custom struct to mirror the JSON data
 type EnemyData struct {
-	ID      int `json:"id"`
-	Animation1Hue int `json:"animation1Hue"`
-	Animation1Name []struct {
-	  `json:"animation1Name"`
-	}
-	Animation2Hue int `json:"animation2Hue"`
-	Animation2Name []struct {
-		`json:"animation2Name"`
-	}
-    Frames int {
-		`json:"frames"`
-	}
-    Name []struct {
-		`json:"name"`
-	}
-	Position int {
-		`json:"position"`
-	}
-	
-	type Timings struct {
-		FlashColor int {
-			`json:"flashColor"`
-		}
-		FlashDuration int {
-			`json:"flashScope"`
-		}
-		Frame int {
-			`json:"frame"`
-		}
-		type Se struct {
-			Name []struct {
-				`json:"name"`
-			}
-			Pan int {
-				`json:"pan"`
-
-			}
-			Pitch int {
-				`json:"pitch"`
-			}
-			Volume int {
-				`json:"volume"`
-			}
-
-		}
-	}
+	ID             int        `json:"id"`
+	Animation1Hue  int        `json:"animation1Hue"`
+	Animation1Name string     `json:"animation1Name"`
+	Animation2Hue  int        `json:"animation2Hue"`
+	Animation2Name string     `json:"animation2Name"`
+	Frames         [][][]int  `json:"frames"`
+	Name           string     `json:"name"`
+	Position       int        `json:"position"`
+	Timings        []Timings  `json:"timings"`
 }
+
+type Timings struct {
+	FlashColor   []int      `json:"flashColor"`
+	FlashDuration int       `json:"flashDuration"`
+	FlashScope    int        `json:"flashScope"`
+	Frame         int        `json:"frame"`
+	Se            Se         `json:"se"`
+}
+
+type Se struct {
+	Name   string `json:"name"`
+	Pan    int    `json:"pan"`
+	Pitch  int    `json:"pitch"`
+	Volume int    `json:"volume"`
+}
+
 
 func main() {
 	// Command-line flags
