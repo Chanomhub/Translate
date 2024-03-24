c3f2e1253ab0ca9ef0571edc19c52cee3167a20c
Chanomhub <121768693+Chanomhub@users.noreply.github.com>
2024-03-24 15:15:32 +0700

    Update main.go

--- RPGM/Chanomhub-RPGM/main.go
+++ RPGM/Chanomhub-RPGM/main.go
@@ -11,30 +11,30 @@ import (
 )
 
 type EnemyData struct {
-	ID             int           `json:"id"`
-	Animation1Hue  int           `json:"animation1Hue"`
-	Animation1Name string        `json:"animation1Name"`
-	Animation2Hue  int           `json:"animation2Hue"`
-	Animation2Name string        `json:"animation2Name"`
-	Frames         [][][]float64 `json:"frames"`
-	Name           string        `json:"name"`
-	Position       int           `json:"position"`
-	Timings        []Timings     `json:"timings"`
+	ID       int      `json:"id"`
+	Animation1Hue int      `json:"animation1Hue"`
+	Animation1Name string    `json:"animation1Name"`
+	Animation2Hue int      `json:"animation2Hue"`
+	Animation2Name string    `json:"animation2Name"`
+	Frames     [][][]float64 `json:"frames"`
+	Name      string    `json:"name"`
+	Position    int      `json:"position"`
+	Timings    []Timings   `json:"timings"`
 }
 
 type Timings struct {
-	FlashColor    []int `json:"flashColor"`
-	FlashDuration int   `json:"flashDuration"`
-	FlashScope    int   `json:"flashScope"`
-	Frame         int   `json:"frame"`
-	Se            Se    `json:"se"`
+	FlashColor  []int `json:"flashColor"`
+	FlashDuration int  `json:"flashDuration"`
+	FlashScope  int  `json:"flashScope"`
+	Frame     int  `json:"frame"`
+	Se      Se  `json:"se"`
 }
 
 type Se struct {
-	Name   string `json:"name"`
-	Pan    int    `json:"pan"`
-	Pitch  int    `json:"pitch"`
-	Volume int    `json:"volume"`
+	Name  string `json:"name"`
+	Pan  int  `json:"pan"`
+	Pitch int  `json:"pitch"`
+	Volume int  `json:"volume"`
 }
 
 // Define a map to associate file names with struct types
@@ -46,7 +46,7 @@ var structMap = map[string]interface{}{
 func main() {
 	// Command-line flags
 	filePath := flag.String("file", "", "Path to the input JSON file")
-	targetLang := flag.String("lang", "es", "Target language code (e.g., 'es' for Spanish)")
+	targetLang := flag.String("lang", "th", "Target language code (e.g., 'th' for Thai)")
 	flag.Parse()
 
 	// Error handling for flags
@@ -123,3 +123,4 @@ func translateAndUpdateNames(enemies []EnemyData, targetLang string) {
 		}
 	}
 }
+
