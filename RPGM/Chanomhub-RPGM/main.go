cabd772c8e3871977f583d8835d55b9bb068794f
Chanomhub <121768693+Chanomhub@users.noreply.github.com>
2024-03-24 07:27:53 +0700

    Update main.go

--- RPGM/Chanomhub-RPGM/main.go
+++ RPGM/Chanomhub-RPGM/main.go
@@ -10,43 +10,41 @@ import (
 	gt "github.com/bas24/googletranslatefree"
 )
 
+// Function to translate the JSON (remains mostly the same)
 func translateJSON(data interface{}, targetLanguage string) (interface{}, error) {
-    switch v := data.(type) {
-    case map[string]interface{}: // If it's a map (object)
-        for key, value := range v {
-            if key == "name" {
-                translated, err := gt.Translate(value.(string), "auto", targetLanguage)
-                if err != nil {
-                    return nil, fmt.Errorf("error translating 'name': %w", err)
-                }
-                v[key] = translated
-            } else {
-                // Recursive call for nested structures
-                translatedValue, err := translateJSON(value, targetLanguage)
-                if err != nil {
-                    return nil, err
-                }
-                v[key] = translatedValue
-            }
-        }
-        return v, nil
-    case []interface{}: // If it's an array
-        for i, value := range v {
-            translatedValue, err := translateJSON(value, targetLanguage)
-            if err != nil {
-                return nil, err
-            }
-            v[i] = translatedValue
-        }
-        return v, nil
-    default: // Base case for other data types
-        return data, nil
-    }
+	switch v := data.(type) {
+	case map[string]interface{}:
+		for key, value := range v {
+			if key == "name" {
+				translated, err := gt.Translate(value.(string), "auto", targetLanguage)
+				if err != nil {
+					return nil, fmt.Errorf("error translating 'name': %w", err)
+				}
+				v[key] = translated
+			} else {
+				translatedValue, err := translateJSON(value, targetLanguage)
+				if err != nil {
+					return nil, err
+				}
+				v[key] = translatedValue
+			}
+		}
+	case []interface{}:
+		for i, value := range v {
+			translatedValue, err := translateJSON(value, targetLanguage)
+			if err != nil {
+				return nil, err
+			}
+			v[i] = translatedValue
+		}
+	default:
+		// No translation needed for other types
+		return data, nil
+	}
+	return data, nil
 }
 
-
 func main() {
-	// Define flags for input and output file names
 	inputFileName := flag.String("input", "", "Input JSON file name")
 	flag.Parse()
 
@@ -55,83 +53,72 @@ func main() {
 		return
 	}
 
-	// 1. Read the JSON from your file
+	// 1. Open both the input JSON and the configuration file
 	jsonFile, err := os.Open(*inputFileName)
 	if err != nil {
-		fmt.Println("Error opening file:", err)
+		fmt.Println("Error opening input JSON file:", err)
 		return
 	}
 	defer jsonFile.Close()
 
+	configureFile, err := os.Open("Configure.json")
+	if err != nil {
+		fmt.Println("Error opening configuration file:", err)
+		return
+	}
+	defer configureFile.Close()
+
+	// 2. Read data from both JSON files
 	jsonData, err := ioutil.ReadAll(jsonFile)
 	if err != nil {
-		fmt.Println("Error reading file:", err)
+		fmt.Println("Error reading input JSON file:", err)
 		return
 	}
 
-	// 2. Unmarshal the JSON
-	var data interface{}
-	err = json.Unmarshal(jsonData, &data)
+	configureData, err := ioutil.ReadAll(configureFile)
 	if err != nil {
-		fmt.Println("Error decoding JSON:", err)
+		fmt.Println("Error reading configuration file:", err)
 		return
 	}
 
-	// 3. Translate
-	translatedData, err := translateJSON(data)
+	// 3. Unmarshal both JSONs
+	var inputData interface{}
+	err = json.Unmarshal(jsonData, &inputData)
+	if err != nil {
+		fmt.Println("Error decoding input JSON:", err)
+		return
+	}
+
+	var config map[string]string
+	err = json.Unmarshal(configureData, &config)
+	if err != nil {
+		fmt.Println("Error decoding configuration JSON:", err)
+		return
+	}
+
+	// 4. Get the target language
+	targetLanguage := config["target_language"]
+
+	// 5. Translate
+	translatedData, err := translateJSON(inputData, targetLanguage)
 	if err != nil {
 		fmt.Println("Error during translation:", err)
 		return
 	}
 
-	// Read the configuration JSON
-configureFile, err := os.Open("Configure.json")
-if err != nil {
-    fmt.Println("Error opening configuration file:", err)
-    return
-}
-defer configureFile.Close()
-
-configureData, err := ioutil.ReadAll(configureFile)
-if err != nil {
-    fmt.Println("Error reading configuration file:", err)
-    return
-}
-
-// Unmarshal the configuration JSON
-var config map[string]string
-err = json.Unmarshal(configureData, &config)
-if err != nil {
-    fmt.Println("Error decoding configuration JSON:", err)
-    return
-}
-
-// Get the target language from the configuration
-targetLanguage := config["target_language"]
-// Translate
-translatedData, err := translateJSON(data, targetLanguage)
-if err != nil {
-    fmt.Println("Error during translation:", err)
-    return
-}
-
-
-	// 4. Re-encode the updated JSON
+	// 6. Re-encode translated JSON
 	updatedJSON, err := json.MarshalIndent(translatedData, "", " ")
 	if err != nil {
 		fmt.Println("Error encoding JSON:", err)
 		return
 	}
 
-	// Get the output file name from the input file name
-	outputFileName := *inputFileName
-
-	// 5. Write back to the file (overwriting it)
-	err = ioutil.WriteFile(outputFileName, updatedJSON, 0644)
+	// 7. Write updated JSON to file
+	err = ioutil.WriteFile(*inputFileName, updatedJSON, 0644)
 	if err != nil {
 		fmt.Println("Error writing file:", err)
 		return
 	}
 
-	fmt.Println("Updated JSON saved to file:", outputFileName)
+	fmt.Println("Updated JSON saved to file:", *inputFileName)
 }
